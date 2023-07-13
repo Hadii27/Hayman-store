aab07c96cb606d263d928f705b3c24c202207f8e
Hadii27 <hadomohamed11@gmail.com>
2023-07-13 04:12:46 +0300

    sdsa

--- HaymanStore/Screens/Main.Designer.cs
+++ HaymanStore/Screens/Main.Designer.cs
@@ -213,7 +213,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.label1.Location = new System.Drawing.Point(449, 199);
+            this.label1.Location = new System.Drawing.Point(447, 190);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(331, 136);
             this.label1.TabIndex = 30;
